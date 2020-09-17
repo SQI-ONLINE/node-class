3b718c9f7bed759fdb400314e977fb7dd90cac46
Favour Afolabi <afolabifavouroluwatobi@gmail.com>
2020-09-17 13:27:12 +0100

    changed route

--- index.js
+++ index.js
@@ -89,80 +89,80 @@ mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
 
 global.User = User;
 
-app.get('/', (req, res) => {
-    res.render('pages/signup')
-})
+// app.get('/', (req, res) => {
+//     res.render('pages/signup')
+// })
 
-app.get('/signin', (req, res) => {
-    res.render('pages/login')
-})
+// app.get('/signin', (req, res) => {
+//     res.render('pages/login')
+// })
 
-app.post('/signup', (req, res) => {
-    const { firstName, lastName, email, phone, password } = req.body;
-    let new_user = { firstName, lastName, email, phone, password };
-    new_user = new User(new_user);
-    new_user.save();
+// app.post('/signup', (req, res) => {
+//     const { firstName, lastName, email, phone, password } = req.body;
+//     let new_user = { firstName, lastName, email, phone, password };
+//     new_user = new User(new_user);
+//     new_user.save();
 
-    res.render('pages/signup', {message: "Signed up successfully "})
-})
+//     res.render('pages/signup', {message: "Signed up successfully "})
+// })
 
-app.get('/dashboard', (req, res) => {
-    res.render('pages/dashboard');
-})
+// app.get('/dashboard', (req, res) => {
+//     res.render('pages/dashboard');
+// })
 
-app.post('/signin', (req, res, next) => {
-    console.log("REACHING HERE");
-})
+// app.post('/signin', (req, res, next) => {
+//     console.log("REACHING HERE");
+// })
 
-const userRoute = require('./routes/userRoute')
-app.use('/user', userRoute)
-app.post('/addUser', (req, res) => {
-    console.log(req.body)
-    let response = {success: true, message: "Reaching Endpoin"}
-    res.send(response)
-})
+// const userRoute = require('./routes/userRoute')
+// app.use('/user', userRoute)
+// app.post('/addUser', (req, res) => {
+//     console.log(req.body)
+//     let response = {success: true, message: "Reaching Endpoin"}
+//     res.send(response)
+// })
 
-app.get('/users', (req, res) => {
+// app.get('/users', (req, res) => {
 
-    let author_id = 1; // 1; drop authors
+//     let author_id = 1; // 1; drop authors
 
-    try {
-        con.query(`SELECT * FROM authors WHERE author_id = ${mysql.escape(author_id)}`, (err, rows)=>{
-            if(err) throw err;
-        })
-    } catch (error) {
+//     try {
+//         con.query(`SELECT * FROM authors WHERE author_id = ${mysql.escape(author_id)}`, (err, rows)=>{
+//             if(err) throw err;
+//         })
+//     } catch (error) {
         
-    }
+//     }
 
-    con.query(`SELECT * FROM authors WHERE author_id = ?`, [author_id], (err, rows)=>{
-        console.log(rows);
-    })
+//     con.query(`SELECT * FROM authors WHERE author_id = ?`, [author_id], (err, rows)=>{
+//         console.log(rows);
+//     })
 
-    con.query(`INSERT INTO authors (author_name, author_phone) values (${name}, ${phone})`, (err, row)=>{
-        if(err){
-            console.log("")
-        }
-    })
+//     con.query(`INSERT INTO authors (author_name, author_phone) values (${name}, ${phone})`, (err, row)=>{
+//         if(err){
+//             console.log("")
+//         }
+//     })
 
-    let author = {author_name: "David", author_phone: 8090909090}
-    con.query(`INSERT INTO authors SET ?`, author, (err, row)=>{
-        if(err){
-            console.log("")
-        }
-    })
+//     let author = {author_name: "David", author_phone: 8090909090}
+//     con.query(`INSERT INTO authors SET ?`, author, (err, row)=>{
+//         if(err){
+//             console.log("")
+//         }
+//     })
 
-    con.query(`UPDATE authors SET author_name = ?, author_phone = ? WHERE author_id = ?`, [name, phone, author_id], (err, res)=> {
+//     con.query(`UPDATE authors SET author_name = ?, author_phone = ? WHERE author_id = ?`, [name, phone, author_id], (err, res)=> {
 
-    })
+//     })
 
-    con.query(`DELETE FROM authors where author_id = ?`, [author_id], (err, res)=>{
-        // ...
-    })
-    // let users = ["Favour","Ridwan","King D", "Star P", "David", "Atom", "Medilley", "Taiwo"];
+//     con.query(`DELETE FROM authors where author_id = ?`, [author_id], (err, res)=>{
+//         // ...
+//     })
+//     // let users = ["Favour","Ridwan","King D", "Star P", "David", "Atom", "Medilley", "Taiwo"];
 
-    // res.status(401).send(users);
-    // res.end()
-})
+//     // res.status(401).send(users);
+//     // res.end()
+// })
 
 app.get('/', (req, res)=>{
     // console.log(global);
